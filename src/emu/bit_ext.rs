5ecf6a6d79276fe1019588916d4e68b3f4afe23f
39bytes <zhangjeff2014@gmail.com>
2024-05-14 22:13:14 -0400

    clippy

--- src/emu/bit_ext.rs
+++ src/emu/bit_ext.rs
@@ -1,10 +1,10 @@
 pub trait BitExt {
-    fn into_bit(&self) -> u8;
+    fn into_bit(self) -> u8;
 }
 
 impl BitExt for u16 {
-    fn into_bit(&self) -> u8 {
-        if *self > 0 {
+    fn into_bit(self) -> u8 {
+        if self > 0 {
             1
         } else {
             0
