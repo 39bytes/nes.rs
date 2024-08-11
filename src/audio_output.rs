138cce40c9c29dc90b6b77975c48ff629b34ea08
39bytes <zhangjeff2014@gmail.com>
2024-08-11 18:00:07 -0400

    add volume adjust to audio output

--- src/audio_output.rs
+++ src/audio_output.rs
@@ -1,3 +1,4 @@
+use crate::extension_traits::*;
 use anyhow::{bail, Result};
 use sdl2::audio::{AudioQueue, AudioSpecDesired};
 
@@ -5,43 +6,46 @@ use super::emu::consts::CLOCK_SPEED;
 
 const TIME_PER_CLOCK: f64 = 1.0 / CLOCK_SPEED as f64;
 const SAMPLE_RATE: i32 = 44100;
+const DEFAULT_VOLUME: f32 = 0.1;
 
 pub struct AudioOutput {
+    volume: f32,
+
     acc: f64,
     time_between_samples: f64,
-    queue: AudioQueue<f32>,
-    buffer: [f32; 512],
+    buffer: [f32; 64],
     buffer_sample_index: usize,
+    queue: AudioQueue<f32>,
 }
 
 impl AudioOutput {
     pub fn new(sdl_context: &sdl2::Sdl) -> Result<Self> {
-        let audio = sdl_context.audio().map_err(anyhow::Error::msg)?;
+        let audio = sdl_context.audio().into_anyhow()?;
         if audio.num_audio_playback_devices().is_none() {
             bail!("No audio playback devices found");
         }
-        let default_device = audio
-            .audio_playback_device_name(0)
-            .map_err(anyhow::Error::msg)?;
+        let default_device = audio.audio_playback_device_name(0).into_anyhow()?;
 
         let queue = audio
             .open_queue(
                 Some(default_device).as_deref(),
                 &AudioSpecDesired {
                     freq: Some(SAMPLE_RATE),
-                    samples: Some(128),
-                    channels: Some(2),
+                    samples: Some(512),
+                    channels: Some(1),
                 },
             )
-            .map_err(anyhow::Error::msg)?;
+            .into_anyhow()?;
         queue.resume();
         log::info!("Opened queue with spec: {:?}", queue.spec());
 
         Ok(AudioOutput {
+            volume: DEFAULT_VOLUME,
+
             acc: 0.0,
             time_between_samples: 1.0 / (SAMPLE_RATE as f64),
             queue,
-            buffer: [0.0; 512],
+            buffer: [0.0; 64],
             buffer_sample_index: 0,
         })
     }
@@ -49,9 +53,9 @@ impl AudioOutput {
     pub fn try_push_sample(&mut self, sample: f32) {
         self.acc += TIME_PER_CLOCK;
         while self.acc >= self.time_between_samples {
-            self.buffer[self.buffer_sample_index] = sample;
-            self.buffer[self.buffer_sample_index + 1] = sample;
-            self.buffer_sample_index += 2;
+            let adjusted = sample * self.volume;
+            self.buffer[self.buffer_sample_index] = adjusted;
+            self.buffer_sample_index += 1;
 
             if self.buffer_sample_index == self.buffer.len() {
                 if let Err(e) = self.queue.queue_audio(&self.buffer) {
@@ -62,5 +66,9 @@ impl AudioOutput {
 
             self.acc -= self.time_between_samples;
         }
+    }
+
+    pub fn set_volume(&mut self, volume: f32) {
+        self.volume = volume;
     }
 }
