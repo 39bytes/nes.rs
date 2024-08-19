f187ab52a41d4b5165fdc3a2d68c7c09d141a777
39bytes <zhangjeff2014@gmail.com>
2024-08-19 01:09:15 -0400

    trying again with vite-static-copy plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,39 @@
 import wasm from "vite-plugin-wasm";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   base: "/nes.rs/",
   build: {
     target: "esnext",
   },
-  plugins: [wasm()],
+  plugins: [
+    wasm(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "coi-serviceworker.min.js",
+          dest: "./",
+        },
+      ],
+    }),
+    {
+      name: "build-html",
+      apply: "build",
+      transformIndexHtml: (html) => {
+        return {
+          html,
+          tags: [
+            {
+              tag: "script",
+              attrs: {
+                src: "/coi-serviceworker.min.js",
+              },
+              injectTo: "head",
+            },
+          ],
+        };
+      },
+    },
+  ],
 });
